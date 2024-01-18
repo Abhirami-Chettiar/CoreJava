f566de95ac39b7e44ec10041af62cc303fc7fbfc
lenovo <lenovo@lenovo-PC-13>
2024-01-18 13:28:53 +0530

    First Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,28 @@
+package com.edubridge.compileinput;
+// Program to demonstrate on CompileTime data types
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte num1 = 127;
+		short num2 = 3433;
+		int num3 = 345671116;
+		long num4 = 98765432123456L;
+		String name= "Abhirami";
+		char c = 'k';
+		float num5 =54.657894325f;
+		double num6 = 54.657894325d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(name);
+		System.out.println(c);
+		System.out.println(num5);
+		System.out.println(num6);
+		
+		
+
+	}
+
+}
